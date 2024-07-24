2b0d93d7f1334b03b9afedc7fab7df3723223b34
bastndev <gohit24@gmail.com>
2024-07-24 18:02:44 -0500

    feat: Add functions for greeting everyone and two

---
+++ class_0/04_functions.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  print(greetEveryone());
+  print(greetTwo());
+}
+
+String greetEveryone() {
+  return "Hello Everyone";
+}
+
+String greetTwo()=> "Hello Everyone 2"; // Short hand syntax for functions
