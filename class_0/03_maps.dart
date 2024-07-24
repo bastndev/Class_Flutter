73fbc176f38f68fa02cdd3a2e1f6cfc6266282f3
bastndev <gohit24@gmail.com>
2024-07-24 17:11:45 -0500

    feat: Add Pokemon data to maps.dart

--- class_0/03_maps.dart
+++ class_0/03_maps.dart
@@ -1,3 +1,32 @@
+/* void main(List<String> args) {
+  final pokemons = {
+    1: 'ABC',
+    2: 'xyz',
+    3: '123',
+    4: 'AB22C',
+  };
+} */
+
+/* void main(List<String> args) {
+  final Map<String, dynamic> pokemon = {
+    'name': 'Ditto',
+    'hp': 100,
+    'isAlive': true,
+    'moves': ['Transform', 'Slam', 'Pound'],
+  };
+
+  print(pokemon);
+}
+ */
+
 void main(List<String> args) {
-  
+  final Map<String, dynamic> pokemon = {
+    'name': 'Ditto',
+    'hp': 100,
+    'isAlive': true,
+    'abilities': <String>['Transform'],
+    'sprites': <int, dynamic>{1: 'ditto/front.png', 2: 'ditto/back.png'}
+  };
+
+  print('Name: ${pokemon['sprites'][1]}');
 }
