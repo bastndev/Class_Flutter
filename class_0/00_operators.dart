3739670253666699b0f106293f89badf7c660449
bastndev <gohit24@gmail.com>
2024-07-26 15:37:12 -0500

    feat: Add new operators and translations for class_0 code examples

--- class_0/00_operators.dart
+++ class_0/00_operators.dart
@@ -1,8 +1,24 @@
 // Operador asignado
-void main(List<String> args) {
+/* void main(List<String> args) {
   int a = 3;
   a += 5;
 
   print(a); // 8
-}
+} */
 
+/* void main(List<String> args) {
+  int a = 3;
+  a -= 5;
+
+  print(a); // -2
+}
+ */
+
+// ternary OPERATOR
+void main(List<String> args) {
+  int a = 2;
+
+  final result = a > 5 ? 'Mayor que 5' : 'Menor o igual a 5';
+  print(result); // Menor o igual a 5
+
+}
