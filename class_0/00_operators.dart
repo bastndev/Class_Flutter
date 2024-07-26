b0e737880bf9f04c6d1116b41b284dfbef350c53
bastndev <gohit24@gmail.com>
2024-07-26 16:27:06 -0500

    feat: Update class_0/00_operators.dart with null safety and example usage

--- class_0/00_operators.dart
+++ class_0/00_operators.dart
@@ -15,10 +15,17 @@
  */
 
 // ternary OPERATOR
-void main(List<String> args) {
+/* void main(List<String> args) {
   int a = 2;
 
   final result = a > 5 ? 'Mayor que 5' : 'Menor o igual a 5';
   print(result); // Menor o igual a 5
 
+}
+ */
+
+void main(List<String> args) {
+  String? name = 'Maria'; // This will cause an error because null safety is enforced in Dart.
+  String realName = name ?? 'Unknown = null'; // RealName is 'Unknown'
+  print(realName);
 }
