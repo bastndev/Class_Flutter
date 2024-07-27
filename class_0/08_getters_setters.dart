93df26ccfb826fb914e976c2954449234720676e
bastndev <gohit24@gmail.com>
2024-07-27 15:12:53 -0500

    Refactor Square class to use getters and setters for side property

--- class_0/08_getters_setters.dart
+++ class_0/08_getters_setters.dart
@@ -1,14 +1,23 @@
 class Square {
-  double side;
-  Square({
-    required this.side,
-  });
+  double _side;
 
-  double calculateArea() => side * side;
+  Square({required double side}) : _side = side;
+
+  double get area {
+    return _side * _side;
+  }
+
+  set side(double value) {
+    print('setting new value: $value');
+    if (value < 0) throw 'Error Enter a positive value';
+
+    _side = value;
+  }
 }
 
 void main(List<String> args) {
   final mySquare = Square(side: 10);
+  mySquare.side = -3;
 
-  print('Area: ${mySquare.calculateArea().toInt()}');
+  print('Area: ${mySquare.area}');
 }
