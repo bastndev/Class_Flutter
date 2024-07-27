1bfa909969c3f3c2043256d9df103b70250f1bfb
bastndev <gohit24@gmail.com>
2024-07-27 16:05:40 -0500

    feat: Add SolarPlant class and changePhone function

--- class_0/09_abstract_class.dart
+++ class_0/09_abstract_class.dart
@@ -1,5 +1,3 @@
-void main(List<String> args) {}
-
 enum PlantType { solar, wind, hydro }
 
 abstract class EnergyPlant {
@@ -11,4 +9,30 @@ abstract class EnergyPlant {
     required this.type,
   });
   void consumeEnergy(double amount);
+}
+
+class SolarPlant extends EnergyPlant {
+  SolarPlant({
+    required double initialEnergy,
+  }) : super(
+          energyLeft: initialEnergy,
+          type: PlantType.solar,
+        );
+
+  @override
+  void consumeEnergy(double amount) {
+    energyLeft -= amount;
+  }
+}
+
+double changePhone(EnergyPlant plant) {
+  if (plant.energyLeft < 10) {
+    throw Exception('Energy is low');
+  }
+  return plant.energyLeft - 20;
+}
+
+void main(List<String> args) {
+  final solarPlant = SolarPlant(initialEnergy: 100);
+  print('Solar: ${changePhone(solarPlant).toInt()}% ');
 }
