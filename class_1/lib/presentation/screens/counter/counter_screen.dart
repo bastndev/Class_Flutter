4ed73fc4567e51c2aff5553a07fb467b20d3aa2f
bastndev <gohit24@gmail.com>
2024-08-09 15:42:49 -0500

    [change] of statless to statlFul

--- class_1/lib/presentation/screens/counter/counter_screen.dart
+++ class_1/lib/presentation/screens/counter/counter_screen.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int number = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -10,19 +17,23 @@ class CounterScreen extends StatelessWidget {
         title: const Text('Counter Screen'),
         centerTitle: true,
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('10',
-                style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100)),
-            Text('Clicks', style: TextStyle(fontSize: 25)),
+            Text('$number',
+                style: const TextStyle(fontSize: 150, fontWeight: FontWeight.w100)),
+            const Text('Clicks', style: TextStyle(fontSize: 25)),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            number++;
+          });
+        },
+        child: const Icon(Icons.plus_one),
       ),
     );
   }
