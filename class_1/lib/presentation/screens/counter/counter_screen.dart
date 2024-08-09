19e5c6739d1b591cc3ad8ace1b172e256b7df25d
bastndev <gohit24@gmail.com>
2024-08-09 16:19:33 -0500

    [add] conter return and minu

--- class_1/lib/presentation/screens/counter/counter_screen.dart
+++ class_1/lib/presentation/screens/counter/counter_screen.dart
@@ -16,24 +16,60 @@ class _CounterScreenState extends State<CounterScreen> {
       appBar: AppBar(
         title: const Text('Counter Screen'),
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                number = 0;
+              });
+            },
+            icon: const Icon(Icons.refresh),
+          ),
+        ],
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text('$number',
-                style: const TextStyle(fontSize: 150, fontWeight: FontWeight.w100)),
-            const Text('Clicks', style: TextStyle(fontSize: 25)),
+                style: const TextStyle(
+                    fontSize: 150, fontWeight: FontWeight.w100)),
+            Text('Click${number == 1 ? '' : 's'}',
+                style: const TextStyle(fontSize: 25)),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            number++;
-          });
-        },
-        child: const Icon(Icons.plus_one),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                number = 0;
+              });
+            },
+            child: const Icon(Icons.refresh),
+          ),
+          const SizedBox(height: 10),
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                number++;
+              });
+            },
+            child: const Icon(Icons.plus_one),
+          ),
+          const SizedBox(height: 10),
+          FloatingActionButton(
+            onPressed: () {
+              if (number == 0) return;
+              setState(() {
+                number--;
+              });
+            },
+            child: const Icon(Icons.exposure_minus_1),
+          ),
+        ],
       ),
     );
   }
