7eed34918ae119cd0ddd781ff2eb405bdd9b5f27
bastndev <gohit24@gmail.com>
2024-08-12 17:38:52 -0500

    ⬆️ [add] ListView to project class

--- class_2/lib/presentation/chat/chat_screen.dart
+++ class_2/lib/presentation/chat/chat_screen.dart
@@ -15,6 +15,31 @@ class ChatScreen extends StatelessWidget {
         ),
         title: const Text('Lucia 💙'),
       ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  return Text('Text $index');
+                },
+              ),
+            ),
+            const Text('Chat View'),
+          ],
+        ),
+      ),
     );
   }
 }
