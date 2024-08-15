6f08c2622653e83d7cce6a1abd75ea511579ea22
bastndev <gohit24@gmail.com>
2024-08-15 16:04:46 -0500

    chore: Add ChatProvider to MultiProvider in main.dart

--- app_1_yes_no/lib/main.dart
+++ app_1_yes_no/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:class_2/config/theme/app_theme.dart';
+import 'package:class_2/presentation/providers/chat_provider.dart';
 import 'package:class_2/presentation/screen/chat/chat_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main(List<String> args) {
   runApp(const MyApp());
@@ -11,11 +13,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      // only have 5 themes
-      theme: AppTheme(selectorColor: 3).theme(),
-      home: const ChatScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ChatProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        // only have 5 themes
+        theme: AppTheme(selectorColor: 3).theme(),
+        home: const ChatScreen(),
+      ),
     );
   }
 }
