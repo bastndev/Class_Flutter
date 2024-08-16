7e46609db4dc443f997f1d2948318805d2258a88
bastndev <gohit24@gmail.com>
2024-08-16 17:34:25 -0500

    [better] dio fromJson

--- app_1_yes_no/lib/config/theme/helpers/get_yes_no_answer.dart
+++ app_1_yes_no/lib/config/theme/helpers/get_yes_no_answer.dart
@@ -1,4 +1,5 @@
 import 'package:class_2/domain/entities/message.dart';
+import 'package:class_2/infrastructure/models/yes_no_model.dart';
 import 'package:dio/dio.dart';
 
 class GetYesNoAnswer {
@@ -7,10 +8,12 @@ class GetYesNoAnswer {
   Future<Message> getAnswer() async {
     final response = await _dio.get('https://yesno.wtf/api');
 
+    final YesNoModel yesNoModel = YesNoModel.fromJson(response.data);
+
     return Message(
-      text: response.data['answer'],
+      text: yesNoModel.answer,
       fromWho: FromWho.her,
-      imgUrl: response.data['image'],
+      imgUrl: yesNoModel.image,
     );
   }
 }
