4425a732abee54c2ad0b4d6c90d7f2bf7d8952c4
bastndev <gohit24@gmail.com>
2024-08-21 17:21:22 -0500

    ⚙️ [add] new Menu item in config

---
+++ widgets_app/lib/config/menu/menu_items.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem({
+    required this.title,
+    required this.subTitle,
+    required this.link,
+    required this.icon,
+  });
+}
+
+const appMenuItems = <MenuItem>[
+  MenuItem(
+    title: 'Buttons',
+    subTitle: 'Different types of buttons',
+    link: '/buttons',
+    icon: Icons.smart_button,
+  ),
+  MenuItem(
+    title: 'Card',
+    subTitle: 'Different types of cards',
+    link: '/cards',
+    icon: Icons.card_giftcard,
+  ),
+];
--- widgets_app/lib/main.dart
+++ widgets_app/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: 3).getTheme(),
+      theme: AppTheme(selectedColor: 0).getTheme(),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Flutter APP'),
