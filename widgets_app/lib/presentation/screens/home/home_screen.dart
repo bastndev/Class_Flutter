19cd80e4c9f65319ecd80892ed3d646ecca486a5
bastndev <gohit24@gmail.com>
2024-08-22 14:54:10 -0500

    [add] listView to project

--- widgets_app/lib/presentation/screens/home/home_screen.dart
+++ widgets_app/lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,6 +10,23 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter + materia 3'),
       ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+// --- --- -- body 
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final menuItem = appMenuItems[index];
+        return Text(menuItem.title);
+      },
     );
   }
 }
