2378b74921a32b71bf756f745a6c00d4052c9ad8
bastndev <gohit24@gmail.com>
2024-08-27 17:34:41 -0500

    feat: Add cards data to CardsScreen

--- widgets_app/lib/presentation/screens/card/cards_screen.dart
+++ widgets_app/lib/presentation/screens/card/cards_screen.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
+const cards = <Map<String,dynamic>>[
+  {'elevation': 0.0, 'label': 'Elevation 0'},
+  {'elevation': 1.0, 'label': 'Elevation 1'},
+  {'elevation': 2.0, 'label': 'Elevation 2'},
+  {'elevation': 3.0, 'label': 'Elevation 3'},
+  {'elevation': 4.0, 'label': 'Elevation 4'},
+  {'elevation': 5.0, 'label': 'Elevation 5'},
+];
+
+
+
 class CardsScreen extends StatelessWidget {
   const CardsScreen({super.key});
 
@@ -7,7 +18,7 @@ class CardsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter + materia 3'),
+        title: const Text('CardsView '),
       ),
       body: const Placeholder(),
     );
