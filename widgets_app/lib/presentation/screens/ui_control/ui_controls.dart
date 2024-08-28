db54dcca2fd0c8717108582f6dbe1c97613a432e
bastndev <gohit24@gmail.com>
2024-08-28 16:37:27 -0500

    [Add] new screens for animated, app tutorial, infinite scroll, progress, snackbar, and UI controls

---
+++ widgets_app/lib/presentation/screens/ui_control/ui_controls.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UiControls extends StatelessWidget {
+
+  static const name = 'ui_controls';
+
+  const UiControls({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+      
+    );
+  }
+}
