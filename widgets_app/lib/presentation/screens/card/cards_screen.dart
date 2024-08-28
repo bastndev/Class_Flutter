2da81b599dc8ca56f8d91ee9943a6e1bc239975a
bastndev <gohit24@gmail.com>
2024-08-28 15:18:13 -0500

    Refactor CardsScreen to use CardType3 and CardType4

--- widgets_app/lib/presentation/screens/card/cards_screen.dart
+++ widgets_app/lib/presentation/screens/card/cards_screen.dart
@@ -31,19 +31,17 @@ class _CardsView extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         children: [
-          ...cards.map(
-            (card) => CardType1(
-              elevation: card['elevation'],
-              label: card['label'],
-            ),
-          ),
-          ...cards.map(
-            (card) => CardType2(
-              elevation: card['elevation'],
-              label: card['label'],
-            ),
-          ),
-          const SizedBox(height: 50,)
+          ...cards.map((card) =>
+              CardType1(elevation: card['elevation'], label: card['label'])),
+          ...cards.map((card) =>
+              CardType2(elevation: card['elevation'], label: card['label'])),
+          ...cards.map((card) =>
+              CardType3(elevation: card['elevation'], label: card['label'])),
+          ...cards.map((card) =>
+              CardType4(elevation: card['elevation'], label: card['label'])),
+          const SizedBox(
+            height: 50,
+          )
         ],
       ),
     );
@@ -86,7 +84,6 @@ class CardType1 extends StatelessWidget {
   }
 }
 
-
 class CardType2 extends StatelessWidget {
   final String label;
   final double elevation;
@@ -100,7 +97,6 @@ class CardType2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context).colorScheme.primary;
-
 
     return Card(
       shape: RoundedRectangleBorder(
@@ -128,6 +124,90 @@ class CardType2 extends StatelessWidget {
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class CardType3 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const CardType3({
+    super.key,
+    required this.label,
+    required this.elevation,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context).colorScheme.primary;
+
+    return Card(
+      color: theme,
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.more_vert),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                label,
+                style: const TextStyle(color: Colors.white),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CardType4 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const CardType4({
+    super.key,
+    required this.label,
+    required this.elevation,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      elevation: elevation,
+      child: Stack(
+        children: [
+          Image.network(
+            'https://picsum.photos/id/${elevation.toInt()}/600/350',
+            height: 350,
+            fit: BoxFit.cover,
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Color(0xFFFFFFFF),
+                borderRadius:
+                    BorderRadius.only(bottomLeft: Radius.circular(15)),
+              ),
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.more_vert),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
