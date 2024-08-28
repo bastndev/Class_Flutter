7820c9761b5c8d31b49e23b9090353c188f53175
bastndev <gohit24@gmail.com>
2024-08-28 17:08:28 -0500

    feat: Add ProgressScreen with CircularProgressIndicator

--- widgets_app/lib/presentation/screens/progress/progress_screen.dart
+++ widgets_app/lib/presentation/screens/progress/progress_screen.dart
@@ -1,15 +1,48 @@
 import 'package:flutter/material.dart';
 
 class ProgressScreen extends StatelessWidget {
-
-  // static const name = 'progress_screen';
-
   const ProgressScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ProgressView '),
+      ),
+      body: const _ProgressView(),
     );
+  }
+}
+
+class _ProgressView extends StatelessWidget {
+  const _ProgressView();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Column(
+        children: [
+          SizedBox(height: 50),
+          Text('CircularProgressIndicator'),
+          SizedBox(height: 10),
+          CircularProgressIndicator(
+              strokeWidth: 5, backgroundColor: Colors.grey),
+          //-- -- --
+          SizedBox(height: 20),
+          Text('CircularProgressIndicator'),
+          SizedBox(height: 10),
+          _ControllerProgressIndicator(),
+        ],
+      ),
+    );
+  }
+}
+
+class _ControllerProgressIndicator extends StatelessWidget {
+  const _ControllerProgressIndicator();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
