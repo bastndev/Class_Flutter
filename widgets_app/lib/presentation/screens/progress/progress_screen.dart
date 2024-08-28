bd0dbde79504c8b6146129dbcbab3fa5f8fc1239
bastndev <gohit24@gmail.com>
2024-08-28 17:45:04 -0500

    feat: Add lazy loading for images in ProgressScreen

--- widgets_app/lib/presentation/screens/progress/progress_screen.dart
+++ widgets_app/lib/presentation/screens/progress/progress_screen.dart
@@ -43,6 +43,28 @@ class _ControllerProgressIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return StreamBuilder(
+      stream: Stream.periodic(const Duration(milliseconds: 300), (value) {
+        return (value * 2) / 100;
+      }).takeWhile((value) => value < 100),
+      builder: (context, snapshot) {
+        final progressValue = snapshot.data ?? 0.0;
+
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(
+                  value: progressValue,
+                  strokeWidth: 5,
+                  backgroundColor: Colors.black),
+              const SizedBox(width: 20),
+              Expanded(child: LinearProgressIndicator(value: progressValue)),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
