649c13c12802d46887a8ba38043e50f2e19d234f
bastndev <gohit24@gmail.com>
2024-09-03 18:19:49 -0500

    feat: Refactor AppTutorialScreen to use SliderInfo class for slides

--- widgets_app/lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ widgets_app/lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,5 +1,34 @@
 import 'package:flutter/material.dart';
 
+class SliderInfo {
+  final String title;
+  final String caption;
+  final String imgUrl;
+
+  SliderInfo({
+    required this.title,
+    required this.caption,
+    required this.imgUrl,
+  });
+}
+
+final slides = <SliderInfo>[
+  SliderInfo(
+      title: 'Search food',
+      caption: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      imgUrl: 'assets/images/1.png'),
+  SliderInfo(
+      title: 'Fast delivery',
+      caption:
+          'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      imgUrl: 'assets/images/2.png'),
+  SliderInfo(
+      title: 'Enjoy the food',
+      caption:
+          'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      imgUrl: 'assets/images/3.png'),
+];
+
 class AppTutorialScreen extends StatelessWidget {
   static const name = 'app_tutorial_screen';
 
@@ -10,8 +39,31 @@ class AppTutorialScreen extends StatelessWidget {
     return Scaffold(
       body: PageView(
         physics: const BouncingScrollPhysics(),
-        children: [Image.asset('assets/images/1.png')],
+        children: slides
+            .map((slideData) => _Slide(
+                  title: slideData.title,
+                  caption: slideData.caption,
+                  imgUrl: slideData.imgUrl,
+                ))
+            .toList(),
       ),
     );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final String title;
+  final String caption;
+  final String imgUrl;
+
+  const _Slide({
+    required this.title,
+    required this.caption,
+    required this.imgUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
