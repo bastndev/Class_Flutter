2361317229362109396dcb60104fbe948164af3c
bastndev <gohit24@gmail.com>
2024-09-03 17:21:41 -0500

    feat: Add lazy loading for images in AppTutorialScreen

--- widgets_app/lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
+++ widgets_app/lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 
 class AppTutorialScreen extends StatelessWidget {
-
   static const name = 'app_tutorial_screen';
 
   const AppTutorialScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      
+    return Scaffold(
+      body: PageView(
+        physics: const BouncingScrollPhysics(),
+        children: [Image.asset('assets/images/1.png')],
+      ),
     );
   }
 }
