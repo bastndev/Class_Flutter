0e618d78818be786b85a25d2f83d9407d1d96f49
bastndev <gohit24@gmail.com>
2024-09-04 17:24:39 -0500

    feat: Add lazy loading for images in InfiniteScroll screen

--- widgets_app/lib/presentation/screens/infinite/infinite_scroll.dart
+++ widgets_app/lib/presentation/screens/infinite/infinite_scroll.dart
@@ -12,6 +12,49 @@ class InfiniteScroll extends StatefulWidget {
 
 class _InfiniteScrollState extends State<InfiniteScroll> {
   List<int> imagesIds = [1, 2, 3, 4, 5];
+  final ScrollController _scrollController = ScrollController();
+  bool isLoading = false;
+  bool isMounted = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels >=
+          _scrollController.position.maxScrollExtent - 500) {
+        loadNextPate();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    isLoading = false;
+    super.dispose();
+  }
+
+  Future loadNextPate() async {
+    if (isLoading) return;
+    setState(() {});
+
+    await Future.delayed(const Duration(seconds: 2));
+
+    addFiveImages();
+    isLoading = false;
+
+    //-TODO: Check if the widget is mounted
+    setState(() {});
+  }
+
+  void addFiveImages() {
+    final lastId = imagesIds.last;
+
+    imagesIds.addAll(
+      [1, 2, 3, 4, 5].map((e) => e + lastId),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +65,7 @@ class _InfiniteScrollState extends State<InfiniteScroll> {
         removeTop: true,
         removeBottom: true,
         child: ListView.builder(
+          controller: _scrollController,
           itemCount: imagesIds.length,
           itemBuilder: (context, index) {
             return FadeInImage(
@@ -29,7 +73,6 @@ class _InfiniteScrollState extends State<InfiniteScroll> {
               width: double.infinity,
               height: 300,
               placeholder: const AssetImage('assets/images/jar-loading.gif'),
-              // image: NetworkImage('https://picsum.photos/id/$index/200/300'),
               image: NetworkImage(
                   'https://picsum.photos/id/${imagesIds[index]}/500/300'),
             );
