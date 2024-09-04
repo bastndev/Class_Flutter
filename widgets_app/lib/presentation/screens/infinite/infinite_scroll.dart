a2a9455f6b3632c9935b8adb162c0e96b0bc9b36
bastndev <gohit24@gmail.com>
2024-09-04 16:40:27 -0500

    feat: Add lazy loading for images in InfiniteScroll screen

--- widgets_app/lib/presentation/screens/infinite/infinite_scroll.dart
+++ widgets_app/lib/presentation/screens/infinite/infinite_scroll.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class InfiniteScroll extends StatelessWidget {
+class InfiniteScroll extends StatefulWidget {
   static const name = 'infinite_scroll';
 
   const InfiniteScroll({super.key});
 
   @override
+  State<InfiniteScroll> createState() => _InfiniteScrollState();
+}
+
+class _InfiniteScrollState extends State<InfiniteScroll> {
+  List<int> imagesIds = [1, 2, 3, 4, 5];
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Infinite Scroll'),
+      backgroundColor: Colors.black,
+      body: MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        removeBottom: true,
+        child: ListView.builder(
+          itemCount: imagesIds.length,
+          itemBuilder: (context, index) {
+            return FadeInImage(
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: 300,
+              placeholder: const AssetImage('assets/images/jar-loading.gif'),
+              // image: NetworkImage('https://picsum.photos/id/$index/200/300'),
+              image: NetworkImage(
+                  'https://picsum.photos/id/${imagesIds[index]}/500/300'),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.pop(),
+        child: const Icon(Icons.arrow_back_ios),
       ),
     );
   }
