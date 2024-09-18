ccff1eb9dab347fe7a61f60a8b44e3a3c7dfba2b
bastndev <gohit24@gmail.com>
2024-09-18 14:49:59 -0500

    Refactor CubitCounterScreen to use CounterCubit and display the counter value dynamically

--- flutter_bloc/lib/src/screens/cubit_counter_screen.dart
+++ flutter_bloc/lib/src/screens/cubit_counter_screen.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_project/src/bloc/counter_cubit/counter_cubit.dart';
 
 class CubitCounterScreen extends StatelessWidget {
   const CubitCounterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => CounterCubit(),
+      child: const _CubitCounterView(),
+    );
+  }
+}
+
+class _CubitCounterView extends StatelessWidget {
+  const _CubitCounterView();
+
+  @override
+  Widget build(BuildContext context) {
+    final counterState = context.watch<CounterCubit>().state;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Cubit Counter'),
+        title: Text('Cubit Counter: ${counterState.transactionCount}'),
         actions: [
           IconButton(
             onPressed: () => {},
@@ -15,8 +31,13 @@ class CubitCounterScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const Center(
-        child: Text('Cubit Counter: xxxx'),
+      body: Center(
+        child: BlocBuilder<CounterCubit, CounterState>(
+          // buildWhen: (previous, current) => previous.count != current.count, // optional
+          builder: (context, state) {
+            return Text('Cubit Counter: ${state.count}');
+          },
+        ),
       ),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
