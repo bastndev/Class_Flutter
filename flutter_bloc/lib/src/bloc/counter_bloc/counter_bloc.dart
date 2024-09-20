01b170c382bcd0607c511768b1bdfb55e4701988
bastndev <gohit24@gmail.com>
2024-09-20 18:01:23 -0500

    Refactor CounterBloc to implement event handler for CounterEvent

--- flutter_bloc/lib/src/bloc/counter_bloc/counter_bloc.dart
+++ flutter_bloc/lib/src/bloc/counter_bloc/counter_bloc.dart
@@ -1,13 +1,18 @@
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 
 part 'counter_event.dart';
 part 'counter_state.dart';
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
-  CounterBloc() : super(CounterInitial()) {
-    on<CounterEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  CounterBloc() : super(const CounterState()) {
+    on<CounterIncreased>(_onCounterIncreased);
+  }
+
+  void _onCounterIncreased(CounterIncreased event, Emitter<CounterState> emit) {
+    emit(state.copyWith(
+      count: state.count + event.value,
+      transactionCount: state.transactionCount + 1,
+    ));
   }
 }
--- flutter_bloc/lib/src/bloc/counter_bloc/counter_state.dart
+++ flutter_bloc/lib/src/bloc/counter_bloc/counter_state.dart
@@ -1,10 +1,24 @@
 part of 'counter_bloc.dart';
 
-sealed class CounterState extends Equatable {
-  const CounterState();
-  
-  @override
-  List<Object> get props => [];
-}
+class CounterState extends Equatable {
+  final int count;
+  final int transactionCount;
 
-final class CounterInitial extends CounterState {}
+  const CounterState({
+    this.count = 10,
+    this.transactionCount = 0,
+  });
+
+  CounterState copyWith({
+    int? count,
+    int? transactionCount,
+  }) {
+    return CounterState(
+      count: count ?? this.count,
+      transactionCount: transactionCount ?? this.transactionCount,
+    );
+  }
+
+  @override
+  List<Object> get props => [count, transactionCount];
+}
