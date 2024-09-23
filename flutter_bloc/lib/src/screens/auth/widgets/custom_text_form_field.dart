aabeefcd93647787d5328033c3222542d1cf3b07
bastndev <gohit24@gmail.com>
2024-09-23 16:23:47 -0500

    Refactor app theme to use green as the seed color

--- flutter_bloc/lib/src/screens/auth/widgets/custom_text_form_field.dart
+++ flutter_bloc/lib/src/screens/auth/widgets/custom_text_form_field.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
 class CustomRegisterForm extends StatelessWidget {
-  const CustomRegisterForm({super.key});
+  // aca van las propiedades que se le pueden pasar al widget
+  final String? label;
+  final String? hint;
+  final String? errorMessage;
+  final void Function(String)? onChanged;
+  final String? Function(String?)? validator;
+
+  const CustomRegisterForm({
+    super.key,
+    this.label,
+    this.hint,
+    this.errorMessage,
+    this.onChanged,
+    this.validator,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -9,18 +23,28 @@ class CustomRegisterForm extends StatelessWidget {
 
     final border = OutlineInputBorder(
       borderRadius: BorderRadius.circular(10),
-      borderSide: BorderSide(color: theme),
+      // borderSide: BorderSide(color: theme),
     );
 
     return TextFormField(
       onChanged: (value) {
-        print('Email: $value');
+        // print('Email: $value');
       },
       decoration: InputDecoration(
         labelText: 'Email',
+        hintText: 'Enter your email',
+        hintStyle: const TextStyle(color: Colors.grey),
         labelStyle: TextStyle(color: theme),
         enabledBorder: border,
-        focusedBorder: border,
+        focusedBorder: border.copyWith(borderSide: BorderSide(color: theme)),
+        isDense: true,
+
+        // --- --- --- Error Style
+        errorText: 'Invalid email',
+        errorBorder:
+            border.copyWith(borderSide: const BorderSide(color: Colors.red)),
+        focusedErrorBorder:
+            border.copyWith(borderSide: const BorderSide(color: Colors.red)),
       ),
     );
   }
