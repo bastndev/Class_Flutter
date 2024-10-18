1e49795eb5a12a099447b263b5200ad6e94cbda3
bastndev <gohit24@gmail.com>
2024-10-18 10:42:21 -0500

    🫧 [Refactor] import paths in app_router.dart

--- push_notification/lib/config/routes/app_router.dart
+++ push_notification/lib/config/routes/app_router.dart
@@ -1,5 +1,5 @@
 import 'package:go_router/go_router.dart';
-import 'package:push_notification/src/screens/screens.dart';
+import 'package:push_notification/src/push_notification/presentation/screens/screens.dart';
 
 final appRouter = GoRouter(
   routes: [
