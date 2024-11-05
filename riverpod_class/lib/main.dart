e9fd3ef1bd36cdfaef885205f35a9d0c93b24ab2
bastndev <gohit24@gmail.com>
2024-11-05 17:44:55 -0500

    🏗️ [add] integrate Riverpod for state management in HomeScreen and update main app structure to use ProviderScope

--- riverpod_class/lib/main.dart
+++ riverpod_class/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_class/screens/home/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(child: MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
