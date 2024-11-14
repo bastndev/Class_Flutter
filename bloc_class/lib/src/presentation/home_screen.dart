30429565a0a9fefbf006aefcc42c3c6183b985ec
bastndev <gohit24@gmail.com>
2024-11-14 18:21:47 -0500

    🏗️ [add] refactor HomeScreen to use StatefulWidget and integrate PhrasesRepository for random phrase display

--- bloc_class/lib/src/presentation/home_screen.dart
+++ bloc_class/lib/src/presentation/home_screen.dart
@@ -1,16 +1,52 @@
+import 'package:bloc_class/src/data/phrases_repository.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String phrase = "...";
+  late PhrasesRepository _phrasesRepository;
+
+  @override
+  void initState() {
+    super.initState();
+    _phrasesRepository = PhrasesRepository();
+  }
+
+  void _getRandomPhrases() async {
+    String newPhrase = await _phrasesRepository.aleatoryPhrase();
+    setState(() => phrase = newPhrase);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter APP'),
+        title: const Text("Bloc DEMO"),
       ),
-      body: const Center(
-        child: Text('Hello World!'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              phrase,
+              style: Theme.of(context).textTheme.headlineSmall,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: _getRandomPhrases,
+              child: const Text("Change phrase"),
+            ),
+          ],
+        ),
       ),
     );
   }
